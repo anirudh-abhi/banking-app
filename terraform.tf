dbd7222e73efd6eb40aea04285b57a29902355b9
anirudh-abhi <101712057+anirudh-abhi@users.noreply.github.com>
2024-07-15 20:40:14 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -34,6 +34,7 @@ resource "aws_instance" "testing-server" {
   ami             = "ami-0e41ff7d11ac11810"  # Update with your preferred AMI ID
   instance_type   = "t2.medium"  # Update with your desired instance type
   key_name        = "next one"
+  security_groups = [aws_security_group.web_sg.name]
 
   tags = {
     Name = "testing-server"
